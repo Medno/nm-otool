c772ebcbd9b092652eebdfbd1c9727f85f464ecf
Philippe Chadenier <phil.chadenier@gmail.com>
2019-04-29 15:11:34 +0200

    Move folder of files, improve test's script

--- tests/test_nm_multi_opts.sh
+++ tests/test_nm_multi_opts.sh
@@ -1,65 +1,21 @@
-#!/bin/zsh
+#!/bin/bash
 
-echo "nm -Uu"
-for file in ../corrupted_folder/hell/** ;
-do ;
+export FILES=./corrupted_folder/*
+export OPTIONS=("-U" "-g" "-n" "-p" "-r" "-u" "-j" "-A")
+
+echo "nm without options"
+for file in $FILES
+do
 	echo $file
-	diff <(./ft_nm -Uu $file) <(nm -Uu $file)
-done;
+	diff <(../ft_nm $file) <(nm $file)
+done
 
-echo "nm -g"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -g $file) <(nm -g $file)
-done;
-
-echo "nm -n"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -n $file) <(nm -n $file)
-done;
-
-echo "nm -p"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -p $file) <(nm -p $file)
-done;
-
-echo "nm -r"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -r $file) <(nm -r $file)
-done;
-
-echo "nm -u"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -u $file) <(nm -u $file)
-done;
-
-echo "nm -j"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -j $file) <(nm -j $file)
-done;
-
-echo "nm -A"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -A $file) <(nm -A $file)
-done;
-
-echo "nm -Aj"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -Aj $file) <(nm -Aj $file)
-done;
-
+for opt in ${OPTIONS[@]}
+do
+	echo "nm $opt"
+	for file in $FILES
+	do
+		echo $file
+		diff <(../ft_nm $opt $file) <(nm $opt $file)
+	done
+done
