7dfef5667a17fe821c6311e983d5e50dd406d82f
Philippe Chadenier <phil.chadenier@gmail.com>
2019-04-30 16:40:29 +0200

    Edit nm test

--- tests/test_nm.sh
+++ tests/test_nm.sh
@@ -1,71 +1,33 @@
-#!/bin/zsh
-echo "Basic nm"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm $file) <(nm $file)
-done;
+#!/bin/bash
 
-echo "nm -U"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -U $file) <(nm -U $file)
-done;
+export FILES=./corrupted_folder/*
+export OPTIONS=("-U" "-g" "-n" "-p" "-r" "-u" "-j" "-A")
+export MULTI_OPTIONS=("-U -u" "-U -g" "-U -n" "-U -p" "-U -r" "-U -j" "-U -A" "-g -n" "-g -p" "-g -r" "-g -u" "-g -j" "-g -A" "-n -p" "-n -r" "-n -u" "-n -j" "-n -A" "-p -r" "-p -u" "-p -j" "-p -A" "-r -u" "-r -j" "-r -A" "-u -j" "-u -A" "-j -A")
 
-echo "nm -g"
-for file in ../corrupted_folder/hell/** ;
-do ;
+echo "nm without options"
+for file in $FILES
+do
 	echo $file
-	diff <(./ft_nm -g $file) <(nm -g $file)
-done;
+	diff <(../ft_nm $file) <(nm $file)
+done
 
-echo "nm -n"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -n $file) <(nm -n $file)
-done;
+for opt in ${OPTIONS[@]}
+do
+	echo "nm $opt"
+	for file in $FILES
+	do
+		echo $file
+		diff <(../ft_nm $opt $file) <(nm $opt $file)
+	done
+done
 
-echo "nm -p"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -p $file) <(nm -p $file)
-done;
-
-echo "nm -r"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -r $file) <(nm -r $file)
-done;
-
-echo "nm -u"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -u $file) <(nm -u $file)
-done;
-
-echo "nm -j"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -j $file) <(nm -j $file)
-done;
-
-echo "nm -A"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -A $file) <(nm -A $file)
-done;
-
-echo "nm -Aj"
-for file in ../corrupted_folder/hell/** ;
-do ;
-	echo $file
-	diff <(./ft_nm -Aj $file) <(nm -Aj $file)
-done;
-
+echo "nm with options"
+for opt in "${MULTI_OPTIONS[@]}"
+do
+	echo "nm $opt"
+	for file in $FILES
+	do
+		echo $file
+		diff <(../ft_nm $opt $file) <(nm $opt $file)
+	done
+done
